b9b23053aa6e69718b97b8b7204d8aa594f44d1d
MacanMichis <mtgcenturion@hotmail.com>
2023-03-19 13:21:35 -0300

    Initial commit

---
+++ Twitter_Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter_Clone
+//
+//  Created by MaCanMichis on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
