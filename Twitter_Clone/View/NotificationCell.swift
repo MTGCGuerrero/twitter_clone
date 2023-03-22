d95e989fd390477a94641bf4a7e146f3dfa7d8ca
MacanMichis <mtgcenturion@hotmail.com>
2023-03-22 11:39:07 -0300

    Notifications View

---
+++ Twitter_Clone/View/NotificationCell.swift
@@ -0,0 +1,50 @@
+//
+//  NotificationCell.swift
+//  Twitter_Clone
+//
+//  Created by MaCanMichis on 3/22/23.
+//
+
+import SwiftUI
+
+struct NotificationCell: View {
+    @State var width = UIScreen.main.bounds.width
+    var body: some View {
+        VStack{
+            Rectangle()
+                .frame(width: width, height: 1, alignment: .center)
+                .foregroundColor(.gray)
+                .opacity(0.3)
+            
+            HStack(alignment: .top){
+                Image(systemName: "person.fill")
+                    .resizable()
+                    .foregroundColor(.blue)
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                
+                VStack(alignment: .leading, spacing: 5, content: {
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 36, height: 36)
+                        .cornerRadius(18)
+                })
+                Text("Michisss")
+                    .fontWeight(.bold)
+                    .foregroundColor(.primary)
+                +
+                Text("followed you")
+                    .foregroundColor(.black)
+                Spacer(minLength: 0)
+            }
+            .padding(.leading,30)
+        }
+    }
+}
+
+struct NotificationCell_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationCell()
+    }
+}
