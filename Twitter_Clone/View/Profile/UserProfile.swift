ee4826300ca9960fb7b5a420002879a877fed199
MacanMichis <mtgcenturion@hotmail.com>
2023-03-25 15:06:27 -0300

    Banner part 2 and profile bio

--- Twitter_Clone/View/Profile/UserProfile.swift
+++ Twitter_Clone/View/Profile/UserProfile.swift
@@ -25,10 +25,10 @@ struct UserProfile: View {
                     
                     return AnyView(
                         ZStack{
-                            Image("banner")
+                            Image("landscape")
                                 .resizable()
                                 .aspectRatio(contentMode: .fill)
-                                .frame(width: UIScreen.main.bounds.width, height: minY > 0 ? 0 : 180 + minY, alignment: .center)
+                                .frame(width: UIScreen.main.bounds.width, height: minY > 0 ? 180 + minY : 180, alignment: .center)
                                 .cornerRadius(0)
                             BlurView()
                                 .opacity(blueViewOpacity())
@@ -54,7 +54,54 @@ struct UserProfile: View {
                 .frame(height: 180)
                 .zIndex(1)
                 
-                
+                VStack{
+                    HStack{
+                        Image("logo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 75, height: 75)
+                            .clipShape(Circle())
+                            .padding(8)
+                            .background(Color.white)
+                            .offset(y: offset < 0 ? getOffset() - 20 : 20 )
+                            .scaleEffect(getScale())
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            Text("Edit Profile")
+                                .foregroundColor(.blue)
+                                .padding(.vertical,10)
+                                .padding(.horizontal)
+                                .background(Capsule().stroke(Color.blue,lineWidth: 1.5))
+                        })
+                    }
+                    .padding(.top, -25)
+                    .padding(.bottom, -10)
+                    
+                    VStack(alignment: .leading, spacing: 8, content: {
+                        Text("Michis")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(.gray)
+                        Text("MichisUI")
+                            .foregroundColor(.gray)
+                        Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+                        HStack(spacing: 5, content: {
+                            Text("13")
+                                .foregroundColor(.primary)
+                                .fontWeight(.semibold)
+                            Text("Followers")
+                                .foregroundColor(.gray)
+                            Text("10")
+                                .foregroundColor(.primary)
+                                .fontWeight(.semibold)
+                                .padding(.leading)
+                            Text("Following")
+                                .foregroundColor(.gray)
+                        })
+                    })
+                }
             }
             
             
@@ -70,7 +117,19 @@ struct UserProfile: View {
         let offset = 60 * (progress > 0 && progress <= 1 ? progress:1)
         return offset
     }
-
+    
+    func getOffset() -> CGFloat {
+        let progress = (-offset / 80) * 20
+        return progress <= 20 ? progress : 20
+    }
+    
+    func getScale() -> CGFloat {
+        let progress = -offset / 80
+        let scale = 1.8 - (progress < 1.0 ? progress : 1)
+        return scale < 1 ? scale : 1
+    }
+    
+    
 }
 
 struct UserProfile_Previews: PreviewProvider {
