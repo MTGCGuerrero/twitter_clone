ef46bb783de37fe8b03b8d91578c71c6b3e451bd
MacanMichis <mtgcenturion@hotmail.com>
2023-03-26 11:18:56 -0400

    register view and some components

---
+++ Twitter_Clone/View/Authentication/RegisterView.swift
@@ -0,0 +1,75 @@
+//
+//  RegisterView.swift
+//  Twitter_Clone
+//
+//  Created by MaCanMichis on 3/26/23.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        VStack{
+            ZStack{
+                HStack{
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Cancel")
+                            .foregroundColor(.blue)
+                    })
+                    Spacer()
+                }
+                .padding(.horizontal)
+                Image("Twitter")
+                    .resizable()
+                    .scaledToFill()
+                    .padding(.trailing)
+                    .frame(width: 20, height: 20)
+            }
+            Text("Create your account")
+                .font(.title)
+                .bold()
+                .padding(.top,35)
+            VStack(alignment: .leading, spacing: nil, content: {
+                CustomAuthTextField(placeholder: "Name", text: $name)
+                CustomAuthTextField(placeholder: "Phone number or email", text: $email)
+                SecureAuthTextfield(placeholder: "password", text: $password)
+            })
+            Spacer(minLength: 0)
+            
+            VStack{
+                Rectangle()
+                    .frame(height:1)
+                    .foregroundColor(.gray)
+                HStack{
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Capsule()
+                            .frame(width: 60, height: 30, alignment: .center)
+                            .foregroundColor(Color("bg"))
+                            .overlay(
+                                Text("Next")
+                                    .foregroundColor(.white)
+                            )
+                    })
+                }
+                .padding(.trailing,24)
+            }
+        }
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
---
+++ Twitter_Clone/View/Items/CustomAuthTextField.swift
@@ -0,0 +1,37 @@
+//
+//  CustomAuthTextField.swift
+//  Twitter_Clone
+//
+//  Created by MaCanMichis on 3/26/23.
+//
+
+import SwiftUI
+
+struct CustomAuthTextField: View {
+    var placeholder: String
+    @Binding var text: String
+    var body: some View {
+        VStack{
+            ZStack(alignment: .leading, content: {
+                if text.isEmpty{
+                    Text(placeholder)
+                        .foregroundColor(.gray)
+                }
+                TextField("",text: $text)
+                    .frame(height: 45)
+                    .foregroundColor(Color("bg"))
+            })
+            Rectangle()
+                .frame(width: 1,alignment: .center)
+                .foregroundColor(.gray)
+                .padding(.top,-2)
+        }
+        .padding(.horizontal)
+    }
+}
+
+//struct CustomAuthTextField_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CustomAuthTextField()
+//    }
+//}
